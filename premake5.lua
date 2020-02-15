5be7cf91b734aa0de957e2f3025813fc33487ab2
Dmitry Bagandov <dmitriy.bagandov@gmail.com>
2020-02-15 17:23:12 +0200

    Remove multithreaded warning

--- premake5.lua
+++ premake5.lua
@@ -24,6 +24,7 @@ project "DonutEngine"
     location "DonutEngine"
     kind "SharedLib"
     language "C++"
+	staticruntime "off"
 
     targetdir  ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -56,7 +57,6 @@ project "DonutEngine"
 
     filter "system:windows"
         cppdialect "C++17"
-        staticruntime "On"
         systemversion "latest"
 
         defines
@@ -73,17 +73,17 @@ project "DonutEngine"
     
     filter "configurations:Debug"
         defines "DN_DEBUG"
-		buildoptions "/MDd"
+		runtime "Debug"
         symbols "On"
     
     filter "configurations:Release"
         defines "DN_RELEASE"
-		buildoptions "/MD"
+		runtime "Release"
         optimize "On"
     
     filter "configurations:Dist"
         defines "DN_DIST"
-		buildoptions "/MD"
+		runtime "Release"
         optimize "On"
     
 
@@ -91,6 +91,7 @@ project "Sandbox"
     location "Sandbox"
     kind "ConsoleApp"
     language "C++"
+	staticruntime "off"
 
     targetdir  ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -114,7 +115,6 @@ project "Sandbox"
 
     filter "system:windows"
         cppdialect "C++17"
-        staticruntime "On"
         systemversion "latest"
 
         defines
