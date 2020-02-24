6dcfbffcdb3a0e4abcc69c0cfe6079326cb5fa38
Dmitry Bagandov <dmitriy.bagandov@gmail.com>
2020-02-24 20:25:51 +0200

    Show OpenGL info on startup

--- DonutEngine/src/Donut/Platform/OpenGL/OpenGLContext.cpp
+++ DonutEngine/src/Donut/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace Donut {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		DN_CORE_ASSERT(status, "Failed to intitialize Glad!");
+
+		DN_CORE_INFO("OpenGL Info:");
+		DN_CORE_INFO(" - Vendor    {0}", glGetString(GL_VENDOR));
+		DN_CORE_INFO(" - Renderer  {0}", glGetString(GL_RENDERER));
+		DN_CORE_INFO(" - Version   {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
