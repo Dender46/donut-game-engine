df274d9d0300c35b3238d8037ffd4abcfee2b978
Dmitry Bagandov <dmitriy.bagandov@gmail.com>
2020-03-02 20:43:41 +0200

    Change log->fatal to log->critical because of deprecation

--- DonutEngine/src/Donut/Log.h
+++ DonutEngine/src/Donut/Log.h
@@ -22,15 +22,15 @@ namespace Donut {
 }
 
 // Core log macros
-#define DN_CORE_TRACE(...)     ::Donut::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define DN_CORE_INFO(...)      ::Donut::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define DN_CORE_WARN(...)      ::Donut::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define DN_CORE_ERROR(...)     ::Donut::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define DN_CORE_FATAL(...)     ::Donut::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define DN_CORE_TRACE(...)		::Donut::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define DN_CORE_INFO(...)		::Donut::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define DN_CORE_WARN(...)		::Donut::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define DN_CORE_ERROR(...)		::Donut::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define DN_CORE_CRITICAL(...)	::Donut::Log::GetCoreLogger()->ciritical(__VA_ARGS__)
 
 // Client log macros
-#define DN_TRACE(...)		   ::Donut::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define DN_INFO(...)		   ::Donut::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define DN_WARN(...)		   ::Donut::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define DN_ERROR(...)		   ::Donut::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define DN_FATAL(...)		   ::Donut::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define DN_TRACE(...)			::Donut::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define DN_INFO(...)			::Donut::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define DN_WARN(...)			::Donut::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define DN_ERROR(...)			::Donut::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define DN_CRITICAL(...)		::Donut::Log::GetCoreLogger()->critical(__VA_ARGS__)
