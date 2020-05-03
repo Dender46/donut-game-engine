eca97e89c6eeb357a7d9ec1e28c6ca1e428d6021
Dmitry <dmitriy.bagandov@gmail.com>
2020-05-03 15:14:14 +0300

    Improve OpenGLShader

--- DonutEngine/src/Donut/Platform/OpenGL/OpenGLShader.cpp
+++ DonutEngine/src/Donut/Platform/OpenGL/OpenGLShader.cpp
@@ -67,17 +67,19 @@ namespace Donut {
 
 		const char* typeToken = "#type";
 		size_t typeTokenLength = strlen(typeToken);
-		size_t pos = source.find(typeToken, 0);
+		size_t pos = source.find(typeToken, 0); //Start of shader type declaration line
 		while (pos != std::string::npos)
 		{
-			size_t eol = source.find_first_of("\r\n", pos);
+			size_t eol = source.find_first_of("\r\n", pos); //End of shader type declaration line
 			DN_CORE_ASSERT(eol != std::string::npos, "Syntax error");
-			size_t begin = pos + typeTokenLength + 1;
-			std::string type = source.substr(begin, eol - begin); // getting string of type
+			size_t begin = pos + typeTokenLength + 1; //Start of shader type name (after "#type " keyword)
+			std::string type = source.substr(begin, eol - begin); //Getting string of type
 			DN_CORE_ASSERT(ShaderTypeFromString(type), "Invalid type of shader!");
 
-			size_t nextLinePos = source.find_first_not_of("\r\n", eol);
-			pos = source.find(typeToken, nextLinePos); // looking for next shader type
+			size_t nextLinePos = source.find_first_not_of("\r\n", eol); //Start of shader code after shader type declaration line
+			DN_CORE_ASSERT(nextLinePos != std::string::npos, "Syntax error");
+			pos = source.find(typeToken, nextLinePos); //Looking for next shader type
+			
 			shaderSources[ShaderTypeFromString(type)] =
 				source.substr(nextLinePos, pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos));
 		}
@@ -161,7 +163,10 @@ namespace Donut {
 		}
 
 		for (GLuint id : glShaderIDs)
+		{
 			glDetachShader(m_RendererID, id);
+			glDeleteShader(id);
+		}
 	}
 
 	OpenGLShader::~OpenGLShader()
